cbda33be7bfcb17af815d6a52ec488d5a312d977
KengiTakery0 <74704965+KengiTakery0@users.noreply.github.com>
2021-11-06 19:47:46 +0300

    Created Animation and updated game logic

--- Froppy/Assets/Scripts/Player/Controller.cs
+++ Froppy/Assets/Scripts/Player/Controller.cs
@@ -1,5 +1,11 @@
 using UnityEngine;
 
+enum PlayerState
+{
+    Idle,
+    Run,
+    Jump
+}
 public class Controller : MonoBehaviour
 {
     [SerializeField] private float Speed = 100f;
@@ -7,6 +13,12 @@ public class Controller : MonoBehaviour
 
     private bool isGrounded = false;
     private float horizonInput;
+
+    private PlayerState State
+    {
+        get { return (PlayerState)anim.GetInteger("State"); }
+        set { anim.SetInteger("State", (int)value); }
+    }
 
     private Animator anim;
     private Rigidbody2D body;
@@ -16,7 +28,7 @@ public class Controller : MonoBehaviour
     {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        spritRenderer = GetComponent<SpriteRenderer>();
+        spritRenderer = GetComponentInChildren<SpriteRenderer>();
 
     }
 
@@ -27,31 +39,37 @@ public class Controller : MonoBehaviour
 
     private void Update()
     {
+        if(isGrounded) State = PlayerState.Idle;
+
         if (Input.GetButton("Horizontal")) Moving();
 
-        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded) Jump();
+        if (Input.GetButtonDown("Jump") && isGrounded) Jump();//Debug.Log("OK"); 
     }
 
 
     private void Moving()
     {
+
         horizonInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2((horizonInput * Speed * Time.deltaTime), body.velocity.y);
 
         //Change Direction
         spritRenderer.flipX = horizonInput < 0.1f;
+
+        if (isGrounded) State = PlayerState.Run;
     }
     private void Jump()
     {
-
         body.AddForce(transform.up * JumpForce, ForceMode2D.Impulse);
-
     }
     private void ChecGround()
     {
 
         Collider2D[] col = Physics2D.OverlapCircleAll(transform.position, 0.5f);
         isGrounded = col.Length > 1;
+        if (!isGrounded) State = PlayerState.Jump;
 
     }
+
+    
 }
