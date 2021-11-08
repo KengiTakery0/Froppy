6a21f3efafeb0b93a9181ab3bdc40dba303ec52a
KengiTakery0 <74704965+KengiTakery0@users.noreply.github.com>
2021-11-08 06:54:38 +0300

    Update LevelChosser.cs

--- Froppy/Assets/Scripts/ProgrammLogic/SceneChanging/LevelChosser.cs
+++ Froppy/Assets/Scripts/ProgrammLogic/SceneChanging/LevelChosser.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class LevelChosser : MonoBehaviour
 {
- public void TraininLevel()
+    public void TraininLevel()
     {
         SceneManager.LoadScene("");
     }
